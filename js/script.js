49e10732059fefb8e6b4057497c460520ef6d5e4
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-02 14:34:55 +0200

    [add] variables

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Svolgimento
+
+// Inizializzo le variabili
+
+let multipleBoth = 0; 								//* variabile per memorizzare i numeri trovati multipli di entrambi 5 e 3
+let multipleFive = 0;								//* variabile per memorizzare i numeri trovati multipli di 5
+let multipleThree = 0;								//* variabile per memorizzare i numeri trovati multipli di 3
+let i = 0;											//* variabile per memorizzare i numeri rimanenti 
+
