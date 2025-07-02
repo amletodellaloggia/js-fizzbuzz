a1f47f1eb6fa017fba5a32bcc464d1a5b3ced2bc
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-02 14:50:07 +0200

    [add] if (2)

--- js/script.js
+++ js/script.js
@@ -16,6 +16,10 @@ for (i=1; i<=100; i++){ 							//* i parte da 1, le i sono tra 1 e 100, li scror
 		multipleBoth += i;							//* salvo il risultato nella variabile 
 		console.log(`FizzBuzz`) 					//* stampo 'FizzBuzz' quando il il numero trovato è sia multiplo di 5 che di 3
 	}
+	else if(i % 5 === 0){ 							//* condizione: se il numero è multiplo solo di 5
+		multipleFive += i;							//* salvo il risultato nella variabile
+		console.log(`Buzz`) 						//* stampo 'Buzz' quando il numero trovato è solo multiplo di 5
+	}
 	else											//* condizione: se il numero non rispetta le condizioni precedenti
 	console.log(`${i}`);							//* stampo i numeri senza condizione rimanenti con il loro valore
 }
