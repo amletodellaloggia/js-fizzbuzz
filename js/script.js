9d8666b2a55d18c37666d43189cb41288ee15f7d
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-02 14:36:31 +0200

    [add] for cycle

--- js/script.js
+++ js/script.js
@@ -7,3 +7,11 @@ let multipleFive = 0;								//* variabile per memorizzare i numeri trovati mult
 let multipleThree = 0;								//* variabile per memorizzare i numeri trovati multipli di 3
 let i = 0;											//* variabile per memorizzare i numeri rimanenti 
 
+
+// Faccio partire il ciclo
+
+for (i=1; i<=100; i++){ 							//* i parte da 1, le i sono tra 1 e 100, li scrorro incrementando i
+	console.log(`${i}`);							//* stampo i numeri da 1 a 100
+}
+
+// Fine del ciclo
